75a651cf409033ad72fa470fc43ca2e4f165678b
loinos <PavlovskiyD.V@yandex.ru>
2022-03-20 09:11:27 +0300

    init

---
+++ Controllers/DefaultController.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Threading.Tasks;
+using System.Web.Http;
+
+
+namespace test1.Controllers
+{
+    public class DefaultController : ApiController
+    {
+        [AcceptVerbs("GET")]
+        [Route("test/{id}")]
+        public async Task<IHttpActionResult> test (string id)
+        {
+            //DatabaseConnect.connectToDB();
+            return Ok(new {data = id});
+        }
+    }
+}
